ef51b3ca91626c1d9fd37a691cd243af126e9a2f
roma <roma.leichenko@gmail.com>
2022-10-19 21:51:28 +0300

    HomeWork3

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ if count == 1:
     a = int(a)
     diameter = a * 2
     square = int(math.pi *(a**2))
-    print('Circle: ' f'{a = }; {diameter = } {square = }')
+    print('Circle: ' f'{a = }; {diameter = }, {square = }')
 elif count == 2:
     (a, b) = values
     a = int(a)
     b = int(b)
     perimeter = (a) + (b) * 2
     square = a * b
-    print('Square or rectangle: ' f'{a = },{b = }; {perimeter = } {square = }')
+    print('Square or rectangle: ' f'{a = },{b = }; {perimeter = }, {square = }')
 elif count == 3:
     (a, b, c) = values
     a = int(a)
@@ -25,4 +25,4 @@ elif count == 3:
     perimeter = (a + b + c)
     p = (a + b + c) / 2
     square = int(sqrt(p * (p - a) * (p - b) * (p - c)))
-    print('Triangle: ' f'{a = } , {b = } , {c = }; {perimeter = } , {square = }')
+    print('Triangle: ' f'{a = } , {b = } , {c = }; {perimeter = }, {square = }')
